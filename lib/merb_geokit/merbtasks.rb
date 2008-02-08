6a8e6bfebcc1323f464b6c5b364d78e1efe473f1
dusty.doris <dusty.doris@57b0c1ec-9d45-0410-9b81-6bebb86bfa36>
2008-02-08 16:35:24 +0000

    updated some tasks
    git-svn-id: http://merb-geokit.googlecode.com/svn/trunk/merb_geokit@8 57b0c1ec-9d45-0410-9b81-6bebb86bfa36

--- lib/merb_geokit/merbtasks.rb
+++ lib/merb_geokit/merbtasks.rb
@@ -2,32 +2,32 @@ namespace :geo do
   
   desc "Look up an address using the failover"
   task :multi do
-    puts GeoKit::Geocoders::MultiGeocoder.geocode(ENV["ADDR"]).to_yaml
+    puts GeoKit::Geocoders::MultiGeocoder.geocode(ENV["ADDR"])
   end
   
   desc "Look up an address on google"
   task :google do
-    puts GeoKit::Geocoders::GoogleGeocoder.geocode(ENV["ADDR"]).to_yaml
+    puts GeoKit::Geocoders::GoogleGeocoder.geocode(ENV["ADDR"])
   end
   
   desc "Look up an address on yahoo"
   task :yahoo do
-    puts GeoKit::Geocoders::YahooGeocoder.geocode(ENV["ADDR"]).to_yaml
+    puts GeoKit::Geocoders::YahooGeocoder.geocode(ENV["ADDR"])
   end
   
   desc "Look up an address on us_geocoder"
   task :us do
-    puts GeoKit::Geocoders::UsGeocoder.geocode(ENV["ADDR"]).to_yaml
+    puts GeoKit::Geocoders::UsGeocoder.geocode(ENV["ADDR"])
   end
   
   desc "Look up an address on ca_geocoder"
   task :ca do
-    puts GeoKit::Geocoders::CaGeocoder.geocode(ENV["ADDR"]).to_yaml
+    puts GeoKit::Geocoders::CaGeocoder.geocode(ENV["ADDR"])
   end
   
   desc "Lookup up the address of an IP"
   task :ip do
-    puts GeoKit::Geocoders::IpGeocoder.geocode(ENV["ADDR"]).to_yaml
+    puts GeoKit::Geocoders::IpGeocoder.geocode(ENV["ADDR"])
   end
     
 end
